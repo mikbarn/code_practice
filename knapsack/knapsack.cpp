b048a04914cdfe5028f641ae1db8944f10352069
Barnum, Michael <Michael.Barnum@nielsen.com>
2024-05-08 18:50:47 -0400

    wip

--- knapsack/knapsack.cpp
+++ knapsack/knapsack.cpp
@@ -4,6 +4,7 @@
 #include <cmath>
 #include <cstdlib>
 #include <time.h>
+#include <array>
 
 using namespace std;
 
@@ -14,14 +15,6 @@ using namespace std;
 void die(const char* msg = nullptr) {
     cout << "C'mon man! " << (msg? msg: "\n");
     exit(-1);
-}
-
-float_t randFloat(float_t min = 0.0, float_t max = 1.0) {
-    if (max <= min) {
-        die();
-    }
-    float_t range = max - min;
-    return min + (((float_t)rand() / RAND_MAX) * range);
 }
 
 
@@ -138,7 +131,7 @@ unique_ptr<MemoKey[]> HashMemo::keys() {
         ListNode * ln = &tab[i];
         while (ln != nullptr && ln->key != nullptr) {
             MemoKey m = *(ln->key);
-            printf("Adding item at [%d][%d] %d (%d, %f)\n", i, k, j, m.id, m.f);
+            //printf("Adding item at [%d][%d] %d (%d, %f)\n", i, k, j, m.id, m.f);
             rval[j++] = *(ln->key);
             k++;
             ln = ln->next.get();
@@ -158,7 +151,7 @@ HashMemo::HashMemo(uint32_t target_num_elements) {
     num_elements = 0;
     capacity = 2 * target_num_elements;
     tab = make_unique<ListNode[]>(capacity);
-    collisions = make_unique<uint32_t[]>(capacity);
+    collisions = make_unique<uint32_t[]>(capacity); // list lengths really
 }
 
 float_t& HashMemo::operator[] (const MemoKey &key) {
@@ -175,6 +168,10 @@ float_t& HashMemo::operator[] (const MemoKey &key) {
     int j = 0;
     while (ln != nullptr) {
         if ((ln->key == nullptr) || (*(ln->key) == key)) {
+            if(ln->key == nullptr) {
+                collisions[idx] = (collisions[idx] > j + 1? collisions[idx]: j + 1);
+                num_elements++;
+            }
             ln->key = make_unique<MemoKey>(key);
             return ln->val;
         }
@@ -198,7 +195,7 @@ void HashMemo::showCollisions() {
             printf("\n");
         total += collisions[i];
     }
-    printf("Total entries: %d Num Elements: %d\n", total, num_elements);
+    printf("Total entries: %d Num Elements: %d Capacity: %d\n", total, num_elements, capacity);
     printf("Load Factor: %f\n", (float_t)total / capacity);
 }
 
@@ -224,8 +221,9 @@ void quickHashTest() {
 }
 
 void solveKnapsack(int32_t item, float_t W, const float_t* vals, const float_t* weights, HashMemo &memo) {
-    if(item == 0 || W <= 0.0) {
-        memo[MemoKey{item, W}] = weights[item] <= W ? vals[item]: 0.0;
+    if(item < 0 || W <= 0.0) {
+        // memo[MemoKey{item, W}] = weights[item] <= W ? vals[item]: 0.0;
+        memo[MemoKey{item, W}] = 0.0f;
         return;
     }
 
@@ -249,6 +247,159 @@ void solveKnapsack(int32_t item, float_t W, const float_t* vals, const float_t* 
 
 }
 
+
+struct Test {
+    int sz = 0;
+    float * w = nullptr;
+    float * v = nullptr;
+    int * expected = nullptr;
+    int exp_count = 0;
+    float expected_max = 0;
+    float W;
+
+    Test& operator=(const Test &other) {
+        if (this != &other) {
+            _free();
+            W = other.W;
+            sz = other.sz;
+            w = new float[other.sz];
+            v = new float[other.sz];
+            exp_count = other.exp_count;
+            expected = new int[exp_count];
+            for (int i = 0; i < other.sz; i++) {
+                w[i] = other.w[i];
+                v[i] = other.v[i];
+            }
+            for (int i = 0; i < exp_count; i++) {
+                expected[i] = other.expected[i];
+            }
+        }
+        return *this;
+    }
+
+    ~Test() {
+        _free();
+    }
+    void _free() {
+        if (w)
+            delete w;
+            delete v;
+            delete expected;
+        w = nullptr;
+        v = nullptr;
+        expected = nullptr;
+    }
+};
+
+const int NUM_TESTS = 3;
+
+
+int main() {
+    quickHashTest();
+
+    Test tests[NUM_TESTS] =  {
+            Test{
+                10,
+                new float[]{23.0, 26.0, 20.0, 18.0, 32.0, 27.0, 29.0, 26.0, 30.0, 27.0},
+                new float[]{505.0, 352.0, 458.0, 220.0, 354.0, 414.0, 498.0, 545.0, 473.0, 543.0},
+                new int[]{0,1,2,3,7},
+                5,
+                1270.0f,
+                67.0f
+            },
+            Test{
+                3,
+                new float[]{50, 20, 30},
+                new float[]{5.0f, 10.0f, 15.0f},
+                new int[]{1,2},
+                2,
+                25.0f,
+                50.0f
+            },
+            Test{
+                3,
+                new float[]{50, 51, 54},
+                new float[]{5.0f, 10.0f, 15.0f},
+                new int[]{0},
+                1,
+                5.0f,
+                50.0f
+            }
+    };
+    Test *test;
+    Test t;
+    for (int test_idx = 0; test_idx < NUM_TESTS; test_idx++) {
+        test = &tests[test_idx];
+        t = *test;
+        int n = t.sz;
+        float W = t.W;
+
+        printf("Running test %d! Items: %d, Max Weight: %5.5f Item list: \n", test_idx, n, W);
+        for (int i = 0; i < n; i++) {
+            printf("item[%03d] v=%05.5f w=%05.5f\n", i, t.v[i], t.w[i]);
+        }
+        printf("-----------------\n");
+
+        HashMemo hm(n * 5);
+        solveKnapsack(n-1, W, t.v, t.w, hm);
+        hm.showCollisions();
+
+        auto max_vals = make_unique<float_t[]>(n);
+        printf("Knapsack memo: \n");
+        unique_ptr<MemoKey[]> keys = hm.keys();
+        int hsize = hm.size();
+        HashMemo::sortKeys(keys, 0, hsize-1);
+        for (int i = 0; i < hsize; i++) {
+            if (hm[keys[i]] <= 0.0f) {
+                continue;
+            }
+        max_vals[keys[i].id] = max(max_vals[keys[i].id], hm[keys[i]]);
+        printf("[%03d, %5.5f]: %5.5f\n", keys[i].id, keys[i].f, hm[keys[i]]);
+        }
+
+        float_t val = 0.0;
+
+        auto item_picks = make_unique<uint32_t[]>(n);
+        int item_idx = 0;
+        for (int i = 0; i < n; i++) {
+            printf("[%03d] max: %5.5f\n", i, max_vals[i]);
+            if (max_vals[i] > val) {
+                item_picks[item_idx++] = i;
+                val = max_vals[i];
+            }
+        }
+
+        cout << "Items Expected: ";
+        for (int i = 0; i < t.exp_count; i++) {
+             cout << t.expected[i] << " ";
+        }
+        cout << "\n";
+        cout << "Items taken: ";
+        bool pass = (item_idx) == t.exp_count;
+        for (int i = 0; i < item_idx; i++) {
+            cout << item_picks[i] << " ";
+            pass &= pass & item_picks[i] == t.expected[i];
+
+        }
+        cout << "\n";
+        cout << "Test " << (pass? "PASSED": "FAILED") << " !! " << endl;
+
+    }
+
+
+    return 0;
+}
+
+
+
+
+// float_t randFloat(float_t min = 0.0, float_t max = 1.0) {
+//     if (max <= min) {
+//         die();
+//     }
+//     float_t range = max - min;
+//     return min + (((float_t)rand() / RAND_MAX) * range);
+// }
 // unique_ptr<float_t[]> genRandom(uint32_t n, float_t max_f=10.000f) {
 //     auto vals = make_unique<float_t[]>(n);
 //     for (int i = 0; i < n; i++) {
@@ -256,61 +407,3 @@ void solveKnapsack(int32_t item, float_t W, const float_t* vals, const float_t* 
 //     }
 //     return vals;
 // }
-
-
-
-int main() {
-    quickHashTest();
-
-    const float_t tst_w[10] = {23.0, 26.0, 20.0, 18.0, 32.0, 27.0, 29.0, 26.0, 30.0, 27.0}; // wikipedia vals
-    const float_t tst_v[10] = {505.0, 352.0, 458.0, 220.0, 354.0, 414.0, 498.0, 545.0, 473.0, 543.0};
-
-    uint32_t n = 10;
-    float_t W = 67.0;
-    HashMemo hm(n);
-
-    const float_t *v, *w;
-    v = tst_v;
-    w = tst_w;
-    for (int i = 0; i < n; i++) {
-         printf("item[%03d] v=%05.5f w=%05.5f\n", i, v[i], w[i]);
-    }
-
-    //solveKnapsack(n-1, W, vals, weights, hm);
-    solveKnapsack(9, W, v, w, hm);
-    hm.showCollisions();
-
-    auto max_vals = make_unique<float_t[]>(n);
-    printf("Knapsack memo: \n");
-    unique_ptr<MemoKey[]> keys = hm.keys();
-    int hsize = hm.size();
-    HashMemo::sortKeys(keys, 0, hsize-1);
-    for (int i = 0; i < hsize; i++) {
-        if (hm[keys[i]] <= 0.0f) {
-            continue;
-        }
-       max_vals[keys[i].id] = max(max_vals[keys[i].id], hm[keys[i]]);
-       printf("[%03d, %5.5f]: %5.5f\n", keys[i].id, keys[i].f, hm[keys[i]]);
-    }
-
-    float_t val = 0.0;
-
-    auto item_picks = make_unique<uint32_t[]>(n);
-
-    for (int i = 0; i < n; i++) {
-        printf("[%03d] max: %5.5f\n", i, max_vals[i]);
-        if (max_vals[i] > val) {
-            item_picks[i] = 1;
-            val = max_vals[i];
-        }
-    }
-    cout << "Items taken: ";
-    for (int i = 0; i < n; i++) {
-        if (item_picks[i]) {
-            cout << i << " ";
-        }
-    }
-    cout << endl;
-
-    return 0;
-}
